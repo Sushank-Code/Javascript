cb528b02a69eeb97523b74f6ae950794e8ae1bcc
Sushank-Code <sushanklamsal7979@gmail.com>
2025-02-01 18:30:43 +0545

    Project-4(star _rating)

--- Mini_Projects/Project-4(star_rating)/script.js
+++ Mini_Projects/Project-4(star_rating)/script.js
@@ -2,5 +2,22 @@
 
 // star-rating
 
-let star = document.querySelectorAll(".fa-regular fa-star");
+let star_list = document.querySelectorAll(".fa-regular");
 let rating = document.querySelector(".rating-value");
+// console.log(stars);
+
+
+star_list.forEach((star, i1) => {
+    star.addEventListener("click", () => {   
+        star_list.forEach((star, i2) => {
+            if (i1 >= i2) {
+                // console.log(i1,i2);
+                star.classList.add("active");
+            } else {
+                // console.log(i1,i2);
+                star.classList.remove("active");
+            }
+        });
+        rating.innerText = i1 + 1;
+    })
+});
